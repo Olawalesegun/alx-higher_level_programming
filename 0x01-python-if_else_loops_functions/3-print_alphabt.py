e3a686c0dd736232e0889ab7826f42bfb145a71f
olawalesegun <olawalesegun0@gmail.com>
2022-12-04 17:22:20 +0000

    Writing a program that prints the ASCII alphabet, in lowercase without q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) != 'e' and chr(i) != 'q':
-        print('{:c}'.format(i), end='')
+for i in range(97, 122):
+    if i != ord('e') and i != ord('q'):
+        `print("{}".format(chr(i)), end='')
