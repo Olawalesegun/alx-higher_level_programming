ed684b83737de885a9a8b2a4cf0866ef2b1f9d97
olawalesegun <olawalesegun@gmail.com>
2023-01-03 00:02:42 +0000

    Writingwq an empty class Rectangle that defines a rectangle:

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class."""
+"""An empty class that defines a rectangle"""
 
 
 class Rectangle:
-    """Represent a rectangle."""
+    """In here is an empty class that represents a rectangle"""
     pass
